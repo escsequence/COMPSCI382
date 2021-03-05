8c5715f5a5df7e4f372bb8b260f391be7ea9c23e
James Johnston <jamestheprogrammer@outlook.com>
2021-03-05 16:18:56 -0600

    Assignment 6 - Completed A6. Intergrated queries into assignment6.php. Added in subtitle variable to passed in so more information is provided.

--- assignment6/assignment6.php
+++ assignment6/assignment6.php
@@ -10,17 +10,21 @@
   include('pdo_connect.php');
 
   // Get the mode/genre
-  $mode = isset($_GET['mode']) ? $_GET['mode'] : "";
-  $genre = isset($_GET['genre']) ?  $_GET['genre'] : "";
-  $member_id = isset($_GET['memberId']) ? $_GET['memberId'] : "";
+  $type = isset($_GET['type']) ? strip_tags($_GET['type']) : null;
+  $genre = isset($_GET['genre']) ?  strip_tags($_GET['genre']) : null;
+  $genre2 = isset($_GET['genre2']) ? strip_tags($_GET['genre2']) : null;
+  $id = isset($_GET['id']) ? strip_tags($_GET['id']) : null;
+  $date = isset($_GET['date']) ? strip_tags($_GET['date']) : null;
+  $date2 = isset($_GET['date2']) ? strip_tags($_GET['date2']) : null;
 
   function get_key_from_value($value, $array) {
     return array_search($value, $array);
   }
 
-  function displayArrayData($title, $list, $headers, $error = null) {
+  function displayArrayData($title, $subtitle, $list, $headers, $error = null) {
     // Title of the table being shown
-  	echo "<h3>{$title}</h3>";
+    echo "<h2>{$title}</h2>";
+  	echo "<h6>{$subtitle}</h6>";
 
     // If there are items to show
   	if (count($list) > 0) {
@@ -143,9 +147,132 @@
               Assignment 6
             </div>
             <div class="card-body">
+              <div class="row">
+                <div class="col-3">
+                  <div class="list-group" role="tablist">
+                    <?php
+                      // Print out our links, this shows which one is currently selected atm
+                      echo ($type == null) ? "<a class='list-group-item list-group-item-action active' href='./assignment6.php'>Home</a>" : "<a class='list-group-item list-group-item-action' href='./assignment6.php'>Home</a>";
+                      echo ($type == "members") ? "<a class='list-group-item list-group-item-action active' href='?type=members'>Members</a>" : "<a class='list-group-item list-group-item-action' href='?type=members'>Members</a>";
+                      echo ($type == "movies" && $genre == "Animation") ? "<a class='list-group-item list-group-item-action active' href='?type=movies&genre=Animation'>Animation</a>" : "<a class='list-group-item list-group-item-action' href='?type=movies&genre=Animation'>Animation</a>";
+                      echo ($type == "movies" && $genre == "Sci-Fi") ? "<a class='list-group-item list-group-item-action active' href='?type=movies&genre=Sci-Fi'>Sci-Fi</a>" : "<a class='list-group-item list-group-item-action' href='?type=movies&genre=Sci-Fi'>Sci-Fi</a>";
+                      echo ($type == "rentals" && $genre == null) ? "<a class='list-group-item list-group-item-action active' href='?type=rentals&date=2017-12-31'>Rentals</a>" : "<a class='list-group-item list-group-item-action' href='?type=rentals&date=2017-12-31'>Rentals</a>";
+                      echo ($type == "rentals" && $genre == "Sci-Fi") ? "<a class='list-group-item list-group-item-action active' href='?type=rentals&date=2017-12-31&genre=Sci-Fi'>Sci-Fi Rentals</a>" : "<a class='list-group-item list-group-item-action' href='?type=rentals&date=2017-12-31&genre=Sci-Fi'>Sci-Fi Rentals</a>";
+                      echo ($type == "movies" && $genre == "Drama") ? "<a class='list-group-item list-group-item-action active' href='?type=movies&date=2010&date2=2019&genre=Drama'>Drama</a>" : "<a class='list-group-item list-group-item-action' href='?type=movies&date=2010&date2=2019&genre=Drama'>Dramas</a>";
+                      echo ($type == "rentalreport" && ($genre == null && $id == null && $date == null)) ? "<a class='list-group-item list-group-item-action active' href='?type=rentalreport'>Rental Report</a>" : "<a class='list-group-item list-group-item-action' href='?type=rentalreport'>Rental Report</a>";
+                      echo ($type == "rentalreport" && ($genre != null && $id == null && $date == null)) ? "<a class='list-group-item list-group-item-action active' href='?type=rentalreport&genre=Drama&genre2=Adventure'>Drama/Adventure Rentals</a>" : "<a class='list-group-item list-group-item-action' href='?type=rentalreport&genre=Drama&genre2=Adventure'>Drama/Adventure Rentals</a>";
+                      echo ($type == "rentalreport" && ($genre != null && $id != null)) ? "<a class='list-group-item list-group-item-action active' href='?type=rentalreport&genre=Sci-Fi&id=5'>Sci-Fi Rentals for Juan</a>" : "<a class='list-group-item list-group-item-action' href='?type=rentalreport&genre=Sci-Fi&id=5'>Sci-Fi Rentals for Juan</a>";
+                      echo ($type == "rentalreport" && ($genre != null && $date != null)) ? "<a class='list-group-item list-group-item-action active' href='?type=rentalreport&genre=Drama&date=2019-12-01'>Drama Rental Report</a>" : "<a class='list-group-item list-group-item-action' href='?type=rentalreport&genre=Drama&date=2019-12-01'>Drama Rental Report</a>";
+                    ?>
+
+                  </div>
+                </div>
+                <div class="col-9">
+                  <div class="tab-content">
+                    <?php
+                      $display_db = true;
+
+                       switch ($type) {
+                         case 'members':
+                          $title = "Members";
+                          $subtitle = "Showing all the members in the database.";
+                          $query = "SELECT firstName, lastName, phone FROM members";
+                          $content = getAll($query, $db);
+                          $headers = ["firstName" => "First Name", "lastName" => "Last Name", "phone" => "Phone"];
+                          $error = "There are no members in the database.";
+                          break;
+
+                         case 'movies':
+                          $title = "Movies";
+                          $query_params = Array(":type" => $genre);
+                          // Check to see if the date1 and date2 are set so we know what kind of query this is
+                          if ($date != null && $date2 != null) {
+                            // Not normal, it is containing a date
+
+                            // Sub title contaning the date
+                            $subtitle = "List of $genre movies released between the years $date and $date2.";
+
+                            $query = "SELECT title, type, year FROM movies WHERE type = :type AND year BETWEEN :year1 AND :year2;";
+                            $query_params[":year1"] = $date;
+                            $query_params[":year2"] = $date2;
+                            $headers = ["title" => "Title", "type" => "Type", "year" => "Year"];
+                          } else {
+                            // Normal query without date criteria
+
+                            // Sub title, stripping out any possible html in the query
+                            $subtitle = "List of $genre movies.";
+
+                            // Assemble the query
+                            $query = "SELECT title, year, movieID FROM movies WHERE type = :type";
+                            $headers = ["title" => "Title", "year" => "Year", "movieID" => "Movie ID#"];
+                          }
+                          $content = getAll($query, $db, $query_params);
+                          $error = "There are no movies in the database under that genre.";
+                          break;
+
+                          case 'rentals':
+                           $title = "Rentals";
+                           $query_params = Array(":date" => $date);
+                           if ($genre == null) {
+                             $query = "SELECT m.title, m.type, r.dateOut FROM rentals r INNER JOIN movies m ON m.movieID = r.movieID WHERE r.dateOut < :date;";
+                             $subtitle = "List of all movie rentals checked out before $date.";
+                           } else {
+                             $query = "SELECT m.title, m.type, r.dateOut FROM rentals r INNER JOIN movies m ON m.movieID = r.movieID WHERE r.dateOut > :date AND m.type = :genre;";
+                             $query_params[":genre"] = $genre;
+                             $subtitle = "List of $genre movie rentals checked out before $date.";
+                           }
+                           $content = getAll($query, $db, $query_params);
+                           $headers = ["title" => "Title", "type" => "Type", "dateOut" => "Date Out"];
+                           $error = "There are no rentals in the database that match that criteria.";
+                           break;
+
+                         case 'rentalreport':
+                          $title = "Rental Report";
+                          $headers = ["firstName" => "First Name", "lastName" => "Last Name", "title" => "Title", "type" => "Type", "year" => "Year"];
+                          if ($genre == null) {
+                            $subtitle = "List of all the rentals out and whom rented it.";
+                            $query = "SELECT mb.firstName, mb.lastName, mv.title, mv.type, mv.year FROM members mb LEFT JOIN rentals r USING(memberID) INNER JOIN movies mv ON (r.movieID = mv.movieID) ORDER BY mb.memberID ASC;";
+                            $query_params = null;
+                          } else {
+                            $query_params = [":type" => $genre];
+                            // If there is a second genre provided we want to see items that are (this or that) genre wise.
+                            if ($genre2 != null) {
+                              $subtitle = "List of all the $genre or $genre2 rentals and whom rented it.";
+                              $query = "SELECT mb.firstName, mb.lastName, mv.title, mv.type, mv.year FROM members mb LEFT JOIN rentals r USING(memberID) INNER JOIN movies mv ON (r.movieID = mv.movieID) WHERE mv.type = :type OR mv.type = :type2 ORDER BY mb.memberID ASC;";
+                              $query_params[":type2"] = $genre2;
+                            } else if ($id != null) {
+                              $subtitle = "List of all the $genre rentals for user id $id.";
+                              $query = "SELECT mb.firstName, mb.lastName, mv.title, mv.type, mv.year FROM members mb LEFT JOIN rentals r USING(memberID) INNER JOIN movies mv ON (r.movieID = mv.movieID) WHERE mv.type = :type AND mb.memberID = :memberid;";
+                              $query_params[":memberid"] = $id;
+                            } else if ($date != null) {
+                              $subtitle = "List of all the $genre rentals for all members checked out after $date.";
+                              $query = "SELECT mb.firstName, mb.lastName, mv.title, mv.type, mv.year, r.dateOut FROM members mb LEFT JOIN rentals r USING(memberID) INNER JOIN movies mv ON (r.movieID = mv.movieID) WHERE mv.type = :type AND r.dateOut > :date;";
+                              $query_params[":date"] = $date;
+                              $headers["dateOut"] = "Date Out";
+                            }
+                          }
+                          $content = getAll($query, $db, $query_params);
+                          $error = "No rentals checked out by any users found.";
+                          break;
+
+                         default:
+                          $display_db = false;
+                          break;
+
+                       }
+
+                      if ($display_db) {
+                        displayArrayData($title, $subtitle, $content, $headers, $error);
+                      } else {
+
+                        echo "<h3>Welcome to the Movie Store database!</h3>Please select an option to query our massive collection of movies, rentals, or members.";
+                      }
+                    ?>
+                  </div>
+                </div>
+              </div>
               <?php
-                //echo getAll("SELECT * FROM users", $db);
-                displayArrayData("Members", getAll("SELECT firstName, lastName, phone FROM members", $db), ["firstName" => "First Name", "lastName" => "Last Name", "phone" => "Phone"]);
+                //
               ?>
             </div>
         </div>
